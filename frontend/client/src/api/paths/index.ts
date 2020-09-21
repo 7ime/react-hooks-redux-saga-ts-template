6888a3c0ae41e7375500b4349c41c22ca97165fa
7ime <lopatsin1990@gmail.com>
2020-09-21 15:17:25 +0300

    refactoring

--- frontend/client/src/api/paths/index.ts
+++ frontend/client/src/api/paths/index.ts
@@ -3,10 +3,6 @@ import {configuration} from '../config'
 const base = (rest: string) => `${configuration.remoteApi}/${rest}`
 
 export default class Paths {
-    static People = class {
-        static fetchHuman = (id: number) => base(`people/${id}`)
-        static fetchPeople = () => base(`posts/`)
-    }
 
     static JsonPlaceholder = class {
         static getPosts = () => base(`posts/`)
