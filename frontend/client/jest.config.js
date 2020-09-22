f88d617c9041b9a21e8d101332928180e169dc9f
7ime <lopatsin1990@gmail.com>
2020-09-22 18:30:39 +0300

    test delay fn

--- frontend/client/jest.config.js
+++ frontend/client/jest.config.js
@@ -28,6 +28,6 @@ module.exports = {
         "node",
     ],
     "collectCoverage": true,
-    "coverageReporters": ['clover'],
+    "coverageReporters": ['clover', 'text'],
     "coverageDirectory": './'
 };
