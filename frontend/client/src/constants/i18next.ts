4c995a951f89e593ed6ce61903352518b0829885
time <ivan@zenio.co>
2020-10-04 20:53:30 +0300

    i18next

---
+++ frontend/client/src/constants/i18next.ts
@@ -0,0 +1,6 @@
+export const I18NEXT_LOCAL_STORAGE_LANG_KEY = 'LANG_KEY'
+
+export const I18NEXT_DETECTION_OPTIONS = {
+    cashes: ['localStorage'],
+    lookupLocalStorage: I18NEXT_LOCAL_STORAGE_LANG_KEY
+}
--- frontend/client/src/index.tsx
+++ frontend/client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import i18next from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 
@@ -11,6 +12,13 @@ import getService from './services'
 import ServiceContext from './components/context/service-context'
 import HelmetSet from './components/common/helmet-set'
 import App from './components/app'
+import {I18NEXT_DETECTION_OPTIONS} from '@constants/i18next'
+
+i18next
+    .use(LanguageDetector)
+    .init({
+        detection: I18NEXT_DETECTION_OPTIONS
+    })
 
 ReactDOM.render((
     <Provider store={getAppStore()}>
