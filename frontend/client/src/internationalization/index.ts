fec20bae50639e2cccd7be66db2772b8d797b351
time <ivan@zenio.co>
2020-10-07 11:17:24 +0300

    i18next refactoring

--- frontend/client/src/internationalization/index.ts
+++ frontend/client/src/internationalization/index.ts
@@ -2,7 +2,7 @@ import i18next from 'i18next'
 import {initReactI18next} from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import {EI18nLanguages, LOCAL_STORAGE_LANG_KEY} from '@constants/internationalization'
-import {internationalizationResources} from '@internationalization/resources'
+import {i18nResources} from '@internationalization/resources'
 
 i18next
     .use(initReactI18next)
@@ -16,7 +16,7 @@ i18next
         interpolation: {
             escapeValue: false
         },
-        resources: internationalizationResources
+        resources: i18nResources
     })
 
 export default i18next
--- frontend/client/src/internationalization/resources/index.ts
+++ frontend/client/src/internationalization/resources/index.ts
@@ -6,9 +6,9 @@ import localesWelcomeRU from '@internationalization/locales/ru/welcome.json'
 import localesStorybookEN from '@internationalization/locales/en/storybook.json'
 import localesStorybookRU from '@internationalization/locales/ru/storybook.json'
 
-export type IInternationalizationResources = RequiredFields<EI18nLanguages, RequiredFields<EI18nNameSpaces, Object>>
+export type II18nResources = RequiredFields<EI18nLanguages, RequiredFields<EI18nNameSpaces, Object>>
 
-export const internationalizationResources: IInternationalizationResources = {
+export const i18nResources: II18nResources = {
     [EI18nLanguages.EN]: {
         [EI18nNameSpaces.welcome]: localesWelcomeEN,
         [EI18nNameSpaces.storybook]: localesStorybookEN
